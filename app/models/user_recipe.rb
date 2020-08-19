e5202e1d06a36bf031843b0d9dd7e20c5a3f872a
valnuccio <val.nuccio@gmail.com>
2020-08-19 18:04:46 -0400

    getting there

--- app/models/user_recipe.rb
+++ app/models/user_recipe.rb
@@ -16,11 +16,20 @@ class UserRecipe < ActiveRecord::Base
 
 
     def self.save_and_rate(recipe)
-        binding.pry
-        UserRecipe.find_or_create_by(user_id: CLI.current_user.id, recipe_id: recipe.spoonacular_id)
-        PROMPT.slider("Rating", max: 5, step: 1, symbols: {bullet: "✭"})
-
+        current_recipe_save = UserRecipe.find_or_create_by(user_id: CLI.current_user.id, recipe_id: recipe.spoonacular_id)
+        if made?
+            star_review=PROMPT.slider("Rating", max: 5, step: 1, symbols: {bullet: "✭"})
+            written_review=PROMPT.ask("Tell us what you thought!")
+            current_recipe_save.increment!(:times_made, by = 1)
+            current_recipe_save.update(star_rating: star_review, review: written_review, last_time_made: Date.today)
+        end
+        CLI.welcome_nav_bar
     end
-
+       
+    def self.made?
+        PROMPT.select("Did you make it?") do |menu|
+            menu.choice "Made it!", true
+            menu.choice "Just browsing", false
+          end
+    end
 end
-# ✰✭
